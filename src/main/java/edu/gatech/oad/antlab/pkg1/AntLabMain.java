1ce8221143855304aa4ef064771eeca09631cc12
neil-lhm <renaissancehang@gmail.com>
2014-09-04 16:48:14 -0400

    clean build files and update gtid by mli

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -66,7 +66,7 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  //Person3 replace P3 with your name
 		  //and gburdell3 with your gt id
 		  Person3 p3 = new Person3("Menghang Li");
-		  toPrint += p3.toString("902928233");
+		  toPrint += p3.toString("mli325");
           //Person4 replace P4 with your name
           //and gburdell4 with your gt id
           Person4 p4 = new Person4("Shaohui Xu");
