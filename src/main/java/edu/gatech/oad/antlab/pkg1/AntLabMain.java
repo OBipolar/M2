0fe00b87a6c81a3b39d2533e7cbb5541a4b03117
Sicong Chen <schen.gatech@gmail.com>
2014-09-05 12:24:13 -0400

    Update of schen384 gradle

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -58,7 +58,7 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  //Person1 replace P1 with your name
 		  //and gburdell1 with your gt id
 		  Person1 p1 = new Person1("Sicong Chen");
-		  toPrint += p1.toString("903003167");
+		  toPrint += p1.toString("schen384");
 		  //Person2 replace P2 with your name
 		  //and gburdell with your gt id
 		  Person2 p2 = new Person2("Zixiang Zhu");
