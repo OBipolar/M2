973a94c37f1f53db1f8fc438f001aa0df51e5728
yvonnesjy <stonfish@hotmail.com>
2014-09-05 01:34:08 -0400

    Created jshi74.gradle

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -74,7 +74,7 @@ import edu.gatech.oad.antlab.pkg2.*;
           //Person5 replace P4 with your name
           //and gburdell5 with your gt id
           Person5 p5 = new Person5("Jinyu Shi");
-          toPrint += p5.toString("902987866");
+          toPrint += p5.toString("jshi74");
 
         System.out.println(toPrint);
 
